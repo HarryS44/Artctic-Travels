6c1d5b0f8b3f3f4b7cc56e6f8bdb2b98807b2578
Harry <harrystone042006@outlook.com>
2024-02-17 13:31:26 +0000

    JavaScript In Progress

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 $(document).ready(() => {
-    $("#location-01 h6").click(() => {
-        $(this).removeClass("hidden");
-        $("#locations-hidden").addClass("hidden");
+    $("#location-01").on("click", () => {
+      if ($("#location-01").hasClass("hidden")) {
+        $("#location-01").removeClass("hidden");
+        console.log("Clicked on location-01 h6 element");
+      } else {
+        $("#location-show-01").addClass("hidden");
+        console.log("Clicked on location-show-01 element");
+      }
     });
-});
+  });
+  
