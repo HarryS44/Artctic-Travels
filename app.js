d0cf5d4fd5f77c39a5dcc510fb45deda76db4212
Harry <harrystone042006@outlook.com>
2024-02-18 19:32:31 +0000

    TIdy up

--- app.js
+++ app.js
@@ -8,24 +8,3 @@ $(document).ready(() => {
     console.log("Clicked on location-01 h6 element");
   });
 });
-
-$(document).ready(() => {
-  let currentIndex = 0;
-  const slides = $(".slider img");
-  const totalSlides = slides.length;
-
-  const showSlide = (index) => {
-    slides.hide();
-    slides.eq(index).fadeIn();
-  };
-
-  $("#prevBtn").on("click", () => {
-    currentIndex = currentIndex === 0 ? totalSlides - 1 : currentIndex - 1;
-    showSlide(currentIndex);
-  });
-
-  $("#nextBtn").on("click", () => {
-    currentIndex = currentIndex === totalSlides - 1 ? 0 : currentIndex + 1;
-    showSlide(currentIndex);
-  });
-});
