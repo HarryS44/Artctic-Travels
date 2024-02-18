b3624fb6f928c31324bf21b1701172c414319373
Harry <harrystone042006@outlook.com>
2024-02-18 10:46:01 +0000

    Start Image Slider

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+const showOnClick = "#location-show-01";
+const hideOnClick = "#locations-hide";
+
 $(document).ready(() => {
-    $("#location-01").on("click", () => {
-      if ($("#location-01").hasClass("hidden")) {
-        $("#location-01").removeClass("hidden");
-        console.log("Clicked on location-01 h6 element");
-      } else {
-        $("#location-show-01").addClass("hidden");
-        console.log("Clicked on location-show-01 element");
-      }
-    });
+  $(hideOnClick).on("click", () => {
+    $(showOnClick).removeClass("hidden");
+    $(hideOnClick).addClass("hidden");
+    console.log("Clicked on location-01 h6 element");
   });
-  
+});
+
+$(document).ready(() => {
+  let currentIndex = 0;
+  const slides = $(".slider img");
+  const totalSlides = slides.length;
+
+  const showSlide = (index) => {
+    slides.hide();
+    slides.eq(index).fadeIn();
+  };
+
+  $("#prevBtn").on("click", () => {
+    currentIndex = currentIndex === 0 ? totalSlides - 1 : currentIndex - 1;
+    showSlide(currentIndex);
+  });
+
+  $("#nextBtn").on("click", () => {
+    currentIndex = currentIndex === totalSlides - 1 ? 0 : currentIndex + 1;
+    showSlide(currentIndex);
+  });
+});
