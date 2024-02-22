a8d151f38a2362aca92705c38dd3794576cddb64
Harry <harrystone042006@outlook.com>
2024-02-22 09:29:55 +0000

    Debugginh JavaScript

--- app.js
+++ app.js
@@ -8,3 +8,25 @@ $(document).ready(() => {
     console.log("Clicked on location-01 h6 element.");
   });
 });
+
+$(document).ready(() => {
+  let $active = $('#slider img.active');
+  if ($active.length === 0) $active = $('#slider img:last');
+
+  let $next = $active.next().length ? $active.next() : $('#slider img:first');
+
+  setInterval(() => {
+      $active.fadeOut(3000, () => {
+          $active.removeClass('active');
+          $next.addClass('active');
+          $active = $next;
+
+          // Update $next to the next image after $active
+          $next = $active.next().length ? $active.next() : $('#slider img:first');
+      });
+  }, 3000);
+});
+
+
+
+//Review the abve code
